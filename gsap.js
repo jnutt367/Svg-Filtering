3cab35f365b0092a14baa5f59de4becd12c02a28
Jason Nutt <jnutt367@gmail.com>
2021-05-02 15:20:04 -0400

    new commit for outlaw move

--- gsap.js
+++ gsap.js
@@ -1,3 +1,16 @@
-gsap.fromTo("#ranger", { x: 10 }, { opacity: 1, x: 100 })
-gsap.fromTo("#outlaw", { x: -550, y: -600 }, { x: -80, y: -700, duration: 3, display: "none" }, 0)
-gsap.to("#adjusted-sheriff", { duration: 1, x: 100, y: -366 })
+gsap.registerPlugin(ScrollTrigger)
+
+
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#ranger",
+        start: "top 50%",
+        end: "bottom top",
+        toggleActions: "restart none none reset"
+    }
+})
+
+.fromTo("#ranger", { x: 10 }, { opacity: 1, x: 100 })
+    .fromTo("#outlaw", { x: -550, y: -600 }, { x: -80, y: -700, duration: 3, display: "none" }, 0)
+    .to("#adjusted-sheriff", { duration: 1, x: 100, y: -366 })
