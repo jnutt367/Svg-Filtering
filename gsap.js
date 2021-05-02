d6d074fb32ea4d15c4f07b7b0ecfe80017f81bba
Jason Nutt <jnutt367@gmail.com>
2021-05-02 17:12:31 -0400

    commit for best mobile version

--- gsap.js
+++ gsap.js
@@ -12,5 +12,5 @@ let tl = gsap.timeline({
 })
 
 .fromTo("#ranger", { x: 10 }, { opacity: 1, x: 100 })
-    .fromTo("#outlaw", { x: -550, y: -600 }, { x: -400, y: -700, duration: 1, display: "none" }, 0)
+    .fromTo("#outlaw", { x: -550, y: -600 }, { x: -600, y: -800, duration: 1, display: "none" }, 0)
     .to("#adjusted-sheriff", { duration: 1, x: 100, y: -366 })
